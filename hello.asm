72d579aa1f455cd4d36a78ef38e6cd4bd504a18e
jahjahway <suge15@sina.cn>
2021-11-15 23:10:39 +0800

    hello, world

--- hello.asm
+++ hello.asm
@@ -1,7 +1,21 @@
+mov ax, 3
+int 0x10
 mov ax, 0xb800
 mov ds, ax
 
-mov byte [0], 'T'
+mov byte [0], 'H'
+mov byte [2], 'e'
+mov byte [4], 'l'
+mov byte [6], 'l'
+mov byte [8], 'o'
+mov byte [10], ','
+mov byte [12], ' '
+mov byte [14], 'W'
+mov byte [16], 'o'
+mov byte [18], 'r'
+mov byte [20], 'l'
+mov byte [22], 'd'
+
 halt:
 	jmp halt
 
